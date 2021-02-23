34b74f8f340b09d1e50f960f8b07b096326c2bca
Fabian Schlegel <fabian.schlegel@grayc.de>
2021-02-23 10:03:52 +0100

    Improve Documentation

--- README.md
+++ README.md
@@ -13,18 +13,62 @@ in the lead and your code base is in sync all the time.
 
 ## How?
 
+The plugin scans for all XML files in the given input directory. It parses every file (DOM parsing, so do not put
+extremely large files there) and looks for the nodes listed below. The text content of those matching nodes are
+collected and put together to a Java constant class.
+
+| Constant Type    | XPath                           | Constant Prefix        | Description               |
+|------------------|---------------------------------|------------------------|---------------------------|
+| Binding ID       | //thing-descriptions/@bindingId | BINDING_ID_            | The ID of the binding     |
+| Thing Type ID    | //thing-type/@id                | THING_TYPE_ID_         | The ID of a thing type    |
+| Bridge Type ID   | //bridge-type/@id               | BRIDGE_TYPE_ID_        | The ID of a bridge type   |
+| Channel Type ID  | //channel-type/@id              | CHANNEL_TYPE_ID_       | The ID of a channel       |
+| Channel Group ID | //channel-group-type/@id        | CHANNEL_GROUP_TYPE_ID_ | The ID of a channel group |
+
+## Usage
+
+Plugin Goals:
+
+| Goal                   | Default Phase    | Description                        |
+|------------------------|------------------|------------------------------------|
+| generate-esh-constants | generate-sources | Generates the constants Java class |
+
+Plugin Configuration:
+
+| Configuration   | Default Value              | Description       |
+|-----------------|----------------------------|-------------------|
+| packageName     | -                          | Java package name |
+| className       | -                          | Java class name   |
+| inputDirectory  | src/main/resources/ESH-INF | Input directory   |
+| outputDirectory | target/generated-sources   | Output directory  |
+
+Example:
+
 ```xml
 <plugins>
 	<plugin>
 		<groupId>io.kokuwa.edge</groupId>
 		<artifactId>esh-constants-maven-plugin</artifactId>
 		<version>RELEASE</version>
-		<configuration>
-			<packageName>io.kokuwa.examples</packageName>
-			<className>Constants</className>
-			<inputDirectory>${project.basdir}/src/main/resources/ESH-INF</inputDirectory>
-			<outputDirectory>${project.build.dir}/generated-sources</outputDirectory>
-		</configuration>
+		<executions>
+			<execution>
+				<id>generate-esh-constants</id>
+				<goals>
+					<goal>generate-esh-constants</goal>
+				</goals>
+				<phase>generate-sources</phase>
+				<configuration>
+					<packageName>io.kokuwa.examples</packageName>
+					<className>Constants</className>
+					<inputDirectory>${project.basedir}/src/main/resources/ESH-INF</inputDirectory>
+					<outputDirectory>${project.build.directory}/generated-sources</outputDirectory>
+				</configuration>
+			</execution>
+		</executions>
 	</plugin>
 </plugins>
 ```
+
+## Contribution
+
+Feel free to add every missing constant XPath as a pull request or kick off an issue on every trouble.
--- src/main/java/io/kokuwa/edge/esh/constants/maven/plugin/GenerateConstantsMojo.java
+++ src/main/java/io/kokuwa/edge/esh/constants/maven/plugin/GenerateConstantsMojo.java
@@ -25,7 +25,6 @@ import javax.xml.xpath.XPathFactory;
 import lombok.Setter;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -37,7 +36,9 @@ import org.xml.sax.SAXException;
 import static freemarker.template.Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS;
 
 /**
- * Mojo for generating constant class from "ESH-INF" folder.
+ * Mojo for generating constant class from "ESH-INF" folder. The plugin scans for XML files in the input folder and
+ * looks for a couple of XPath matching nodes to extract constants strings and writes them to a Java constants class
+ * using a Freemarker template.
  *
  * @author Fabian Schlegel
  */
@@ -60,7 +61,10 @@ public class GenerateConstantsMojo extends AbstractMojo {
 	private String className;
 
 	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
+	public void execute() throws MojoExecutionException {
+
+		// Create output directory
+		createOutputDirectory();
 
 		// Get input file list
 		final Set<Path> inputFiles = findXMLFiles();
@@ -93,9 +97,29 @@ public class GenerateConstantsMojo extends AbstractMojo {
 
 	}
 
+	/**
+	 * Creates output directory (recursively)
+	 *
+	 * @throws MojoExecutionException on error, execution fails
+	 */
+	private void createOutputDirectory() throws MojoExecutionException {
+		try {
+			Files.createDirectories(Path.of(this.outputDirectory));
+		} catch (IOException e) {
+			throw new MojoExecutionException("Unable to create output directory: " + this.outputDirectory, e);
+		}
+	}
+
+	/**
+	 * Find all XML files in the input folder.
+	 *
+	 * @return Set of {@link Path} to XML files
+	 * @throws MojoExecutionException on error, execution fails
+	 */
 	private Set<Path> findXMLFiles() throws MojoExecutionException {
 
 		Set<Path> result;
+		// Recursively walk down the directory tree and put all ".xml" files in the set
 		try (Stream<Path> walk = Files.walk(Path.of(inputDirectory))) {
 			result = walk.filter(p -> p.getFileName().toString().endsWith("xml")).collect(Collectors.toSet());
 
@@ -106,6 +130,15 @@ public class GenerateConstantsMojo extends AbstractMojo {
 		return result;
 	}
 
+	/**
+	 * Retrieve constant strings from the given XML file.
+	 *
+	 * @param file       the input file
+	 * @param expression the XPath expression to the constant
+	 * @param prefix     the constants prefix
+	 * @return A {@link Map} of constant name and constant value
+	 * @throws MojoExecutionException on error, execution fails
+	 */
 	private Map<String, String> getConstants(File file, String expression, String prefix)
 			throws MojoExecutionException
 	{
@@ -120,10 +153,12 @@ public class GenerateConstantsMojo extends AbstractMojo {
 
 			final Map<String, String> result = new HashMap<>();
 
+			// Find node list in document matching the xpath expression
 			XPathExpression xPathExpression = xpath.compile(expression);
 			NodeList nodeList = (NodeList) xPathExpression.evaluate(document, XPathConstants.NODESET);
 			for (int i = 0; i < nodeList.getLength(); i++) {
 				Node node = nodeList.item(i);
+				// Put the text content of the node to the map
 				result.put(prefix + node.getTextContent().toUpperCase(), node.getTextContent());
 			}
 			return result;
@@ -138,17 +173,27 @@ public class GenerateConstantsMojo extends AbstractMojo {
 		}
 	}
 
+	/**
+	 * Create the Java class based on the Freemarker template.
+	 *
+	 * @param constants the constants map to generate
+	 * @return the Java class content as string
+	 * @throws MojoExecutionException on error, execution fails
+	 */
 	private String createClassFromTemplate(Map<String, String> constants) throws MojoExecutionException {
 		try {
+			// Template input
 			Map root = new HashMap();
 			root.put("package", this.packageName);
 			root.put("class", this.className);
 			root.put("constants", constants);
-			Configuration cfg = new Configuration(DEFAULT_INCOMPATIBLE_IMPROVEMENTS); // Create configuration
+			// Grep template from classpath
+			Configuration cfg = new Configuration(DEFAULT_INCOMPATIBLE_IMPROVEMENTS);
 			cfg.setClassForTemplateLoading(this.getClass(), "/");
-			Template template = cfg.getTemplate("constants.ftl"); // Filename of your template
-			StringWriter sw = new StringWriter(); // So you can use the output as String
-			template.process(root, sw); // process the template to output
+			Template template = cfg.getTemplate("constants.ftl");
+			// Process template
+			StringWriter sw = new StringWriter();
+			template.process(root, sw);
 			return sw.toString();
 		} catch (TemplateException e) {
 			throw new MojoExecutionException("Unable to generate code from template.", e);
@@ -157,6 +202,13 @@ public class GenerateConstantsMojo extends AbstractMojo {
 		}
 	}
 
+	/**
+	 * Write file content to disk.
+	 *
+	 * @param path    the path to the file
+	 * @param content the file content
+	 * @throws MojoExecutionException on error, execution fails
+	 */
 	private void writeFile(Path path, String content) throws MojoExecutionException {
 		try {
 			Files.write(path, content.getBytes());
