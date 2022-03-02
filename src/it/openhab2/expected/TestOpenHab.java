b0bf4444ce382e14c757e7f552101486d78c5ab6
Stephan Schnabel <stephan.schnabel@posteo.de>
2022-03-02 13:27:57 +0100

    Add simple invoker test. Fix #13

---
+++ src/it/openhab2/expected/TestOpenHab.java
@@ -0,0 +1,42 @@
+package io.kokuwa.test;
+
+import static io.kokuwa.test.TestString.*;
+
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+
+public final class TestOpenHab {
+  public static final ThingTypeUID WMBUSBRIDGE_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, BRIDGE_TYPE_ID_WMBUSBRIDGE);
+
+  public static final ThingTypeUID WMBUSVIRTUALBRIDGE_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, BRIDGE_TYPE_ID_WMBUSVIRTUALBRIDGE);
+
+  public static final ThingTypeUID ITRON_SMOKE_DETECTOR_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, THING_TYPE_ID_ITRON_SMOKE_DETECTOR);
+
+  public static final ThingTypeUID TECHEM_HKV45_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, THING_TYPE_ID_TECHEM_HKV45);
+
+  public static final ThingTypeUID TECHEM_HKV61_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, THING_TYPE_ID_TECHEM_HKV61);
+
+  public static final ThingTypeUID TECHEM_HKV64_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, THING_TYPE_ID_TECHEM_HKV64);
+
+  public static final ThingTypeUID TECHEM_HKV69_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, THING_TYPE_ID_TECHEM_HKV69);
+
+  public static final ThingTypeUID TECHEM_HKV94_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, THING_TYPE_ID_TECHEM_HKV94);
+
+  public static final ThingTypeUID TECHEM_SD76_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, THING_TYPE_ID_TECHEM_SD76);
+
+  public static final ThingTypeUID TECHEM_WZ62_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, THING_TYPE_ID_TECHEM_WZ62);
+
+  public static final ThingTypeUID TECHEM_WZ72_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, THING_TYPE_ID_TECHEM_WZ72);
+
+  public static final ThingTypeUID TECHEM_WMZ43_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, THING_TYPE_ID_TECHEM_WMZ43);
+
+  public static final ThingTypeUID METER_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, THING_TYPE_ID_METER);
+
+  public static final ThingTypeUID ENCRYPTED_METER_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, THING_TYPE_ID_ENCRYPTED_METER);
+
+  public static final ThingTypeUID THING_WITH_GROUPS_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, THING_TYPE_ID_THING_WITH_GROUPS);
+
+  public static final ThingTypeUID EGO_TCP_THING_TYPE_UID = new ThingTypeUID(BINDING_ID, THING_TYPE_ID_EGO_TCP);
+
+  private TestOpenHab() {
+  }
+}
---
+++ src/it/openhab2/expected/TestString.java
@@ -0,0 +1,332 @@
+package io.kokuwa.test;
+
+import java.lang.String;
+
+public final class TestString {
+  public static final String BINDING_ID = "wmbus";
+
+  public static final String PROPERTY_SERIAL = "serial";
+
+  public static final String THING_TYPE_ID_ITRON_SMOKE_DETECTOR = "itron_smoke_detector";
+
+  public static final String THING_TYPE_ID_TECHEM_HKV45 = "techem_hkv45";
+
+  public static final String THING_TYPE_ID_TECHEM_HKV61 = "techem_hkv61";
+
+  public static final String THING_TYPE_ID_TECHEM_HKV64 = "techem_hkv64";
+
+  public static final String THING_TYPE_ID_TECHEM_HKV69 = "techem_hkv69";
+
+  public static final String THING_TYPE_ID_TECHEM_HKV94 = "techem_hkv94";
+
+  public static final String THING_TYPE_ID_TECHEM_SD76 = "techem_sd76";
+
+  public static final String THING_TYPE_ID_TECHEM_WZ62 = "techem_wz62";
+
+  public static final String THING_TYPE_ID_TECHEM_WZ72 = "techem_wz72";
+
+  public static final String THING_TYPE_ID_TECHEM_WMZ43 = "techem_wmz43";
+
+  public static final String THING_TYPE_ID_METER = "meter";
+
+  public static final String THING_TYPE_ID_ENCRYPTED_METER = "encrypted_meter";
+
+  public static final String THING_TYPE_ID_THING_WITH_GROUPS = "thing_with_groups";
+
+  public static final String THING_TYPE_ID_EGO_TCP = "ego-tcp";
+
+  public static final String BRIDGE_TYPE_ID_WMBUSBRIDGE = "wmbusbridge";
+
+  public static final String BRIDGE_TYPE_ID_WMBUSVIRTUALBRIDGE = "wmbusvirtualbridge";
+
+  public static final String CID_CURRENT_DATE = "current_date";
+
+  public static final String CID_CURRENT_DATE_STRING = "current_date_string";
+
+  public static final String CID_CURRENT_DATE_NUMBER = "current_date_number";
+
+  public static final String CID_STATUS_REMOVAL_OCCURRED = "status_removal_occurred";
+
+  public static final String CID_STATUS_BILLING_DATE = "status_billing_date";
+
+  public static final String CID_STATUS_PRODUCT_INSTALLED = "status_product_installed";
+
+  public static final String CID_STATUS_OPERATION_MODE = "status_operation_mode";
+
+  public static final String CID_STATUS_PERIMETER_INTRUSION_OCCURRED = "status_perimeter_intrusion_occurred";
+
+  public static final String CID_STATUS_SMOKE_INLET_BLOCKED_OCCURRED = "status_smoke_inlet_blocked_occurred";
+
+  public static final String CID_STATUS_OUT_OF_TEMP_RANGE_OCCURRED = "status_out_of_temp_range_occurred";
+
+  public static final String CID_STATUS_PRODUCT_CODE = "status_product_code";
+
+  public static final String CID_STATUS_BATTERY_LIFETIME = "status_battery_lifetime";
+
+  public static final String CID_STATUS_PERIMETER_INTRUSION = "status_perimeter_intrusion";
+
+  public static final String CID_STATUS_REMOVAL_ERROR = "status_removal_error";
+
+  public static final String CID_STATUS_DATA_ENCRYPTED = "status_data_encrypted";
+
+  public static final String CID_LAST_SMOKE_ALERT_START_DATE = "last_smoke_alert_start_date";
+
+  public static final String CID_LAST_SMOKE_ALERT_START_DATE_STRING = "last_smoke_alert_start_date_string";
+
+  public static final String CID_LAST_SMOKE_ALERT_START_DATE_NUMBER = "last_smoke_alert_start_date_number";
+
+  public static final String CID_LAST_SMOKE_ALERT_END_DATE = "last_smoke_alert_end_date";
+
+  public static final String CID_LAST_SMOKE_ALERT_END_DATE_STRING = "last_smoke_alert_end_date_string";
+
+  public static final String CID_LAST_SMOKE_ALERT_END_DATE_NUMBER = "last_smoke_alert_end_date_number";
+
+  public static final String CID_LAST_BEEPER_STOPPED_DURING_SMOKE_ALERT_DATE = "last_beeper_stopped_during_smoke_alert_date";
+
+  public static final String CID_LAST_BEEPER_STOPPED_DURING_SMOKE_ALERT_DATE_STRING = "last_beeper_stopped_during_smoke_alert_date_string";
+
+  public static final String CID_LAST_BEEPER_STOPPED_DURING_SMOKE_ALERT_DATE_NUMBER = "last_beeper_stopped_during_smoke_alert_date_number";
+
+  public static final String CID_LAST_PERIMETER_INTRUSION_OBSTACLE_OCCURRED_DATE = "last_perimeter_intrusion_obstacle_occurred_date";
+
+  public static final String CID_LAST_PERIMETER_INTRUSION_OBSTACLE_OCCURRED_DATE_STRING = "last_perimeter_intrusion_obstacle_occurred_date_string";
+
+  public static final String CID_LAST_PERIMETER_INTRUSION_OBSTACLE_OCCURRED_DATE_NUMBER = "last_perimeter_intrusion_obstacle_occurred_date_number";
+
+  public static final String CID_LAST_PERIMETER_INTRUSION_OBSTACLE_REMOVED_DATE = "last_perimeter_intrusion_obstacle_removed_date";
+
+  public static final String CID_LAST_PERIMETER_INTRUSION_OBSTACLE_REMOVED_DATE_STRING = "last_perimeter_intrusion_obstacle_removed_date_string";
+
+  public static final String CID_LAST_PERIMETER_INTRUSION_OBSTACLE_REMOVED_DATE_NUMBER = "last_perimeter_intrusion_obstacle_removed_date_number";
+
+  public static final String CID_LAST_SMOKE_INLET_BLOCKED_DATE = "last_smoke_inlet_blocked_date";
+
+  public static final String CID_LAST_SMOKE_INLET_BLOCKED_DATE_STRING = "last_smoke_inlet_blocked_date_string";
+
+  public static final String CID_LAST_SMOKE_INLET_BLOCKED_DATE_NUMBER = "last_smoke_inlet_blocked_date_number";
+
+  public static final String CID_LAST_SMOKE_INLET_BLOCKING_REMOVED_DATE = "last_smoke_inlet_blocking_removed_date";
+
+  public static final String CID_LAST_SMOKE_INLET_BLOCKING_REMOVED_DATE_STRING = "last_smoke_inlet_blocking_removed_date_string";
+
+  public static final String CID_LAST_SMOKE_INLET_BLOCKING_REMOVED_DATE_NUMBER = "last_smoke_inlet_blocking_removed_date_number";
+
+  public static final String CID_LAST_TEMPERATURE_OUT_OF_RANGE_DATE = "last_temperature_out_of_range_date";
+
+  public static final String CID_LAST_TEMPERATURE_OUT_OF_RANGE_DATE_STRING = "last_temperature_out_of_range_date_string";
+
+  public static final String CID_LAST_TEMPERATURE_OUT_OF_RANGE_DATE_NUMBER = "last_temperature_out_of_range_date_number";
+
+  public static final String CID_LAST_TEST_SWITCH_DATE = "last_test_switch_date";
+
+  public static final String CID_LAST_TEST_SWITCH_DATE_STRING = "last_test_switch_date_string";
+
+  public static final String CID_LAST_TEST_SWITCH_DATE_NUMBER = "last_test_switch_date_number";
+
+  public static final String CID_NUMBER_OF_TEST_SWITCHES_OPERATED = "number_of_test_switches_operated";
+
+  public static final String CID_PERIMETER_INTRUSION_DAY_COUNTER_CUMULATED = "perimeter_intrusion_day_counter_cumulated";
+
+  public static final String CID_SMOKE_INLET_DAY_COUNTER_CUMULATED = "smoke_inlet_day_counter_cumulated";
+
+  public static final String CID_CURRENT_READING = "current_reading";
+
+  public static final String CID_LAST_READING = "last_reading";
+
+  public static final String CID_LAST_DATE = "last_date";
+
+  public static final String CID_LAST_DATE_STRING = "last_date_string";
+
+  public static final String CID_LAST_DATE_NUMBER = "last_date_number";
+
+  public static final String CID_RECEPTION = "reception";
+
+  public static final String CID_ALMANAC = "almanac";
+
+  public static final String CID_ROOM_TEMPERATURE = "room_temperature";
+
+  public static final String CID_RADIATOR_TEMPERATURE = "radiator_temperature";
+
+  public static final String CID_STATUS = "status";
+
+  public static final String CID_CURRENT_DATE_SMOKE = "current_date_smoke";
+
+  public static final String CID_CURRENT_DATE_SMOKE_STRING = "current_date_smoke_string";
+
+  public static final String CID_CURRENT_DATE_SMOKE_NUMBER = "current_date_smoke_number";
+
+  public static final String CID_COUNTER = "counter";
+
+  public static final String CID_LAST_FRAME = "last_frame";
+
+  public static final String CHANNEL_TYPE_ID_ITRON_CURRENT_DATE_TIME = "itron_current_date_time";
+
+  public static final String CHANNEL_TYPE_ID_ITRON_CURRENT_DATE_TIME_STRING = "itron_current_date_time_string";
+
+  public static final String CHANNEL_TYPE_ID_ITRON_CURRENT_DATE_TIME_NUMBER = "itron_current_date_time_number";
+
+  public static final String CHANNEL_TYPE_ID_STATUS_REMOVAL_OCCURRED = "status_removal_occurred";
+
+  public static final String CHANNEL_TYPE_ID_STATUS_BILLING_DATE = "status_billing_date";
+
+  public static final String CHANNEL_TYPE_ID_STATUS_PRODUCT_INSTALLED = "status_product_installed";
+
+  public static final String CHANNEL_TYPE_ID_STATUS_OPERATION_MODE = "status_operation_mode";
+
+  public static final String CHANNEL_TYPE_ID_STATUS_PERIMETER_INTRUSION_OCCURRED = "status_perimeter_intrusion_occurred";
+
+  public static final String CHANNEL_TYPE_ID_STATUS_SMOKE_INLET_BLOCKED_OCCURRED = "status_smoke_inlet_blocked_occurred";
+
+  public static final String CHANNEL_TYPE_ID_STATUS_OUT_OF_TEMP_RANGE_OCCURRED = "status_out_of_temp_range_occurred";
+
+  public static final String CHANNEL_TYPE_ID_STATUS_PRODUCT_CODE = "status_product_code";
+
+  public static final String CHANNEL_TYPE_ID_STATUS_BATTERY_LIFETIME = "status_battery_lifetime";
+
+  public static final String CHANNEL_TYPE_ID_LAST_SMOKE_ALERT_START_DATE = "last_smoke_alert_start_date";
+
+  public static final String CHANNEL_TYPE_ID_LAST_SMOKE_ALERT_START_DATE_STRING = "last_smoke_alert_start_date_string";
+
+  public static final String CHANNEL_TYPE_ID_LAST_SMOKE_ALERT_START_DATE_NUMBER = "last_smoke_alert_start_date_number";
+
+  public static final String CHANNEL_TYPE_ID_LAST_SMOKE_ALERT_END_DATE = "last_smoke_alert_end_date";
+
+  public static final String CHANNEL_TYPE_ID_LAST_SMOKE_ALERT_END_DATE_STRING = "last_smoke_alert_end_date_string";
+
+  public static final String CHANNEL_TYPE_ID_LAST_SMOKE_ALERT_END_DATE_NUMBER = "last_smoke_alert_end_date_number";
+
+  public static final String CHANNEL_TYPE_ID_LAST_BEEPER_STOPPED_DURING_SMOKE_ALERT_DATE = "last_beeper_stopped_during_smoke_alert_date";
+
+  public static final String CHANNEL_TYPE_ID_LAST_BEEPER_STOPPED_DURING_SMOKE_ALERT_DATE_STRING = "last_beeper_stopped_during_smoke_alert_date_string";
+
+  public static final String CHANNEL_TYPE_ID_LAST_BEEPER_STOPPED_DURING_SMOKE_ALERT_DATE_NUMBER = "last_beeper_stopped_during_smoke_alert_date_number";
+
+  public static final String CHANNEL_TYPE_ID_LAST_PERIMETER_INTRUSION_OBSTACLE_OCCURRED_DATE = "last_perimeter_intrusion_obstacle_occurred_date";
+
+  public static final String CHANNEL_TYPE_ID_LAST_PERIMETER_INTRUSION_OBSTACLE_OCCURRED_DATE_STRING = "last_perimeter_intrusion_obstacle_occurred_date_string";
+
+  public static final String CHANNEL_TYPE_ID_LAST_PERIMETER_INTRUSION_OBSTACLE_OCCURRED_DATE_NUMBER = "last_perimeter_intrusion_obstacle_occurred_date_number";
+
+  public static final String CHANNEL_TYPE_ID_LAST_PERIMETER_INTRUSION_OBSTACLE_REMOVED_DATE = "last_perimeter_intrusion_obstacle_removed_date";
+
+  public static final String CHANNEL_TYPE_ID_LAST_PERIMETER_INTRUSION_OBSTACLE_REMOVED_DATE_STRING = "last_perimeter_intrusion_obstacle_removed_date_string";
+
+  public static final String CHANNEL_TYPE_ID_LAST_PERIMETER_INTRUSION_OBSTACLE_REMOVED_DATE_NUMBER = "last_perimeter_intrusion_obstacle_removed_date_number";
+
+  public static final String CHANNEL_TYPE_ID_LAST_SMOKE_INLET_BLOCKED_DATE = "last_smoke_inlet_blocked_date";
+
+  public static final String CHANNEL_TYPE_ID_LAST_SMOKE_INLET_BLOCKED_DATE_STRING = "last_smoke_inlet_blocked_date_string";
+
+  public static final String CHANNEL_TYPE_ID_LAST_SMOKE_INLET_BLOCKED_DATE_NUMBER = "last_smoke_inlet_blocked_date_number";
+
+  public static final String CHANNEL_TYPE_ID_LAST_SMOKE_INLET_BLOCKING_REMOVED_DATE = "last_smoke_inlet_blocking_removed_date";
+
+  public static final String CHANNEL_TYPE_ID_LAST_SMOKE_INLET_BLOCKING_REMOVED_DATE_STRING = "last_smoke_inlet_blocking_removed_date_string";
+
+  public static final String CHANNEL_TYPE_ID_LAST_SMOKE_INLET_BLOCKING_REMOVED_DATE_NUMBER = "last_smoke_inlet_blocking_removed_date_number";
+
+  public static final String CHANNEL_TYPE_ID_LAST_TEMPERATURE_OUT_OF_RANGE_DATE = "last_temperature_out_of_range_date";
+
+  public static final String CHANNEL_TYPE_ID_LAST_TEMPERATURE_OUT_OF_RANGE_DATE_STRING = "last_temperature_out_of_range_date_string";
+
+  public static final String CHANNEL_TYPE_ID_LAST_TEMPERATURE_OUT_OF_RANGE_DATE_NUMBER = "last_temperature_out_of_range_date_number";
+
+  public static final String CHANNEL_TYPE_ID_LAST_TEST_SWITCH_DATE = "last_test_switch_date";
+
+  public static final String CHANNEL_TYPE_ID_LAST_TEST_SWITCH_DATE_STRING = "last_test_switch_date_string";
+
+  public static final String CHANNEL_TYPE_ID_LAST_TEST_SWITCH_DATE_NUMBER = "last_test_switch_date_number";
+
+  public static final String CHANNEL_TYPE_ID_NUMBER_OF_TEST_SWITCHES_OPERATED = "number_of_test_switches_operated";
+
+  public static final String CHANNEL_TYPE_ID_PERIMETER_INTRUSION_DAY_COUNTER_CUMULATED = "perimeter_intrusion_day_counter_cumulated";
+
+  public static final String CHANNEL_TYPE_ID_SMOKE_INLET_DAY_COUNTER_CUMULATED = "smoke_inlet_day_counter_cumulated";
+
+  public static final String CHANNEL_TYPE_ID_LAST_FRAME = "last_frame";
+
+  public static final String CHANNEL_TYPE_ID_ROOM_TEMPERATURE = "room_temperature";
+
+  public static final String CHANNEL_TYPE_ID_RADIATOR_TEMPERATURE = "radiator_temperature";
+
+  public static final String CHANNEL_TYPE_ID_CURRENT_READING = "current_reading";
+
+  public static final String CHANNEL_TYPE_ID_PREVIOUS_READING = "previous_reading";
+
+  public static final String CHANNEL_TYPE_ID_LAST_READING = "last_reading";
+
+  public static final String CHANNEL_TYPE_ID_CURRENT_DATE = "current_date";
+
+  public static final String CHANNEL_TYPE_ID_CURRENT_DATE_STRING = "current_date_string";
+
+  public static final String CHANNEL_TYPE_ID_CURRENT_DATE_NUMBER = "current_date_number";
+
+  public static final String CHANNEL_TYPE_ID_PREVIOUS_DATE = "previous_date";
+
+  public static final String CHANNEL_TYPE_ID_PREVIOUS_DATE_STRING = "previous_date_string";
+
+  public static final String CHANNEL_TYPE_ID_PREVIOUS_DATE_NUMBER = "previous_date_number";
+
+  public static final String CHANNEL_TYPE_ID_LAST_DATE = "last_date";
+
+  public static final String CHANNEL_TYPE_ID_LAST_DATE_STRING = "last_date_string";
+
+  public static final String CHANNEL_TYPE_ID_LAST_DATE_NUMBER = "last_date_number";
+
+  public static final String CHANNEL_TYPE_ID_RECEPTION = "reception";
+
+  public static final String CHANNEL_TYPE_ID_ALMANAC = "almanac";
+
+  public static final String CHANNEL_TYPE_ID_STATUS = "status";
+
+  public static final String CHANNEL_TYPE_ID_COUNTER = "counter";
+
+  public static final String CHANNEL_TYPE_ID_CURRENT_DATE_SMOKE = "current_date_smoke";
+
+  public static final String CHANNEL_TYPE_ID_CURRENT_DATE_SMOKE_STRING = "current_date_smoke_string";
+
+  public static final String CHANNEL_TYPE_ID_CURRENT_DATE_SMOKE_NUMBER = "current_date_smoke_number";
+
+  public static final String CHANNEL_TYPE_ID_ENERGY = "energy";
+
+  public static final String CHANNEL_TYPE_ID_VOLUME = "volume";
+
+  public static final String CHANNEL_TYPE_ID_POWER = "power";
+
+  public static final String CHANNEL_TYPE_ID_FLOW_VOLUME = "flow_volume";
+
+  public static final String CHANNEL_TYPE_ID_FLOW_TEMPERATURE = "flow_temperature";
+
+  public static final String CHANNEL_TYPE_ID_RETURN_TEMPERATURE = "return_temperature";
+
+  public static final String CHANNEL_TYPE_ID_EXTERNAL_TEMPERATURE = "external_temperature";
+
+  public static final String CHANNEL_TYPE_ID_TEMPERATURE_DIFFERENCE = "temperature_difference";
+
+  public static final String CHANNEL_TYPE_ID_MAX_POWER = "max_power";
+
+  public static final String CHANNEL_TYPE_ID_MAX_FLOW_VOLUME = "max_flow_volume";
+
+  public static final String CHANNEL_TYPE_ID_MAX_FLOW_TEMPERATURE = "max_flow_temperature";
+
+  public static final String CHANNEL_TYPE_ID_MAX_RETURN_TEMPERATURE = "max_return_temperature";
+
+  public static final String CHANNEL_TYPE_ID_MAX_EXTERNAL_TEMPERATURE = "max_external_temperature";
+
+  public static final String CHANNEL_TYPE_ID_ON_TIME_ERROR = "on_time_error";
+
+  public static final String CHANNEL_TYPE_ID_ERROR_DATE = "error_date";
+
+  public static final String CHANNEL_TYPE_ID_ERROR_FLAGS = "error_flags";
+
+  public static final String CHANNEL_GROUP_TYPE_ID_GROUP_TYPE_1 = "group_type_1";
+
+  public static final String CHANNEL_GROUP_TYPE_ID_GROUP_TYPE_2 = "group_type_2";
+
+  public static final String GID_GROUP1 = "group1";
+
+  public static final String GID_GROUP2 = "group2";
+
+  private TestString() {
+  }
+}
