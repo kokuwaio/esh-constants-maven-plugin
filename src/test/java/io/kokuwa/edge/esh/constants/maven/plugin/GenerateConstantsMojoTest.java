86cd81bb0fa48c64cd18a87c7e99979aac730f1a
Stephan Schnabel <stephan.schnabel@posteo.de>
2022-11-04 20:28:17 +0100

    Reconfigure checkstyle, remove unused dependency mockito

--- src/test/java/io/kokuwa/edge/esh/constants/maven/plugin/GenerateConstantsMojoTest.java
+++ src/test/java/io/kokuwa/edge/esh/constants/maven/plugin/GenerateConstantsMojoTest.java
@@ -1,55 +1,43 @@
 package io.kokuwa.edge.esh.constants.maven.plugin;
 
-import com.google.testing.compile.JavaFileObjectSubject;
-import com.google.testing.compile.JavaFileObjects;
-import io.kokuwa.edge.esh.constants.maven.plugin.junit.MojoExtension;
-import io.kokuwa.edge.esh.constants.maven.plugin.junit.MojoProperty;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import static com.google.testing.compile.JavaFileObjects.forResource;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static com.google.testing.compile.JavaFileObjects.forResource;
+import org.junit.jupiter.api.Test;
+
+import com.google.testing.compile.JavaFileObjectSubject;
+import com.google.testing.compile.JavaFileObjects;
 
 /**
  * Tests for {@link GenerateConstantsMojo}.
  *
  * @author Fabian Schlegel
  */
-@ExtendWith(MojoExtension.class)
-@MojoProperty(name = "packageName", value = "org.test")
-@MojoProperty(name = "stringsClassName", value = "StringConstants")
-@MojoProperty(name = "openhabClassName", value = "OpenHabConstants")
-@MojoProperty(name = "inputDirectory", value = "src/test/resources/ESH-INF")
-@MojoProperty(name = "outputDirectory", value = "/will/be/set/in/test")
 public class GenerateConstantsMojoTest {
 
 	@Test
-	public void verifyStringsEquals(GenerateConstantsMojo mojo) throws IOException, MojoExecutionException, MojoFailureException {
+	void verify() throws Exception {
+
 		Path outputDirectory = Files.createTempDirectory("esh-constants-test");
+		GenerateConstantsMojo mojo = new GenerateConstantsMojo();
+		mojo.setOpenhabClassName("OpenHabConstants");
+		mojo.setStringsClassName("StringConstants");
+		mojo.setPackageName("org.test");
+		mojo.setInputDirectory("src/test/resources/ESH-INF");
+		mojo.setOutputDirectory("/will/be/set/in/test");
 		mojo.setOutputDirectory(outputDirectory.toString());
 		mojo.execute();
 
-		String constants = new String(Files.readAllBytes(Path.of(outputDirectory.toString(), "org", "test", "StringConstants.java")));
-
-		JavaFileObjectSubject.assertThat(JavaFileObjects.forSourceString("org.test.StringConstants", constants))
+		String constants = new String(Files.readAllBytes(outputDirectory.resolve("org/test/StringConstants.java")));
+		JavaFileObjectSubject
+				.assertThat(JavaFileObjects.forSourceString("org.test.StringConstants", constants))
 				.hasSourceEquivalentTo(forResource("ExpectedStringConstants.java"));
-	}
 
-
-	@Test
-	public void verifyOpenHabEquals(GenerateConstantsMojo mojo) throws IOException, MojoExecutionException, MojoFailureException {
-		Path outputDirectory = Files.createTempDirectory("esh-constants-test");
-		mojo.setOutputDirectory(outputDirectory.toString());
-		mojo.execute();
-
-		String ohConstants = new String(Files.readAllBytes(Path.of(outputDirectory.toString(), "org", "test", "OpenHabConstants.java")));
-
-		JavaFileObjectSubject.assertThat(JavaFileObjects.forSourceString("org.test.OpenHabConstants", ohConstants))
+		String ohConstants = new String(Files.readAllBytes(outputDirectory.resolve("org/test/OpenHabConstants.java")));
+		JavaFileObjectSubject
+				.assertThat(JavaFileObjects.forSourceString("org.test.OpenHabConstants", ohConstants))
 				.hasSourceEquivalentTo(forResource("ExpectedOpenHabConstants.java"));
 	}
 }
